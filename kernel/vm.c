3403e5d9e843efba548db8334f37b6a6d580aaf4
李君硕 <2454337@example.com>
2026-07-07 14:05:03 +0800

    finish pgtbl vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,35 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+static void
+vmprintwalk(pagetable_t pagetable, int depth)
+{
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+
+    if(pte & PTE_V){
+      uint64 pa = PTE2PA(pte);
+
+      for(int j = 0; j <= depth; j++){
+        if(j > 0)
+          printf(" ");
+        printf("..");
+      }
+
+      printf("%d: pte %p pa %p\n", i, pte, pa);
+
+      if((pte & (PTE_R | PTE_W | PTE_X)) == 0){
+        vmprintwalk((pagetable_t)pa, depth + 1);
+      }
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprintwalk(pagetable, 0);
+}
